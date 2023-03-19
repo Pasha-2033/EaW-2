19e5025d19f5d33378db1b9ffa97eea6483acb79
Павел <pablo.sokolov@mail.ru>
2023-03-19 22:30:56 +0300

    5.1.4->5.1.1 (crashes, cloader is in lua5.1.a?)

--- common/defines/virus_importer.lua
+++ common/defines/virus_importer.lua
@@ -1,7 +1,14 @@
 function virus_loader()
     --%USERPROFILE% -why not works?
-    package.path = package.path..";C:\\Users\\User\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\EaW 2\\common\\defines"..";C:\\Users\\User\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\EaW 2\\common\\defines\\?.dll"..";C:\\Users\\User\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\EaW 2\\common\\defines\\?.lua"
+    package.path = package.path..";C:\\Users\\User\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\EaW 2\\common\\defines\\?.lua"
+    --package.cpath = package.cpath..";C:\\Users\\User\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\EaW 2\\common\\defines\\?.dll"
     require("virus")
-    --virus.createfile()
+    virus.createfile()
+    
+    
+
+    --path = "C:\\Users\\User\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod\\EaW 2\\common\\defines\\virus.dll"
+    --loadlib(path, "init")()
+    --create_file()
 end
 virus_loader()
